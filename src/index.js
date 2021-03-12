b88d7094309bdbcba0c6943b0d1f667f5f2501cd
Desiree Lerma <desi.lerma96@yahoo.com>
2021-03-12 03:39:08 -0600

    commit tree b330203a121f5ca14faad8aa7d8ab63b690c2737

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import manageUsers from './reducers/manageUsers'
 
 
 // add imports and code
-
+// create store takes in our reducer as an arg and redux dev tools
+let store = createStore(manageUsers, window.__REUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   // add imports and code
-  <App />
+  // pass store as prop
+  <Provider store = { store }>
+    <App />
+  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
